4fd12727ddd5fc42c18ec14475f5e3e22b4e984b
khaitiger <38967901+khaitiger@users.noreply.github.com>
2021-05-27 23:28:42 +0700

    update template

--- src/main/java/Mlem/com/Common/Controllers/HomeController.java
+++ src/main/java/Mlem/com/Common/Controllers/HomeController.java
@@ -44,6 +44,57 @@ public class HomeController {
 		
 	 return "index";
 	}
+	@RequestMapping("listcourse")
+	public String Listcourse(@CookieValue(value = "MY_USER", defaultValue = "defaultCookieValue") String userCookie,
+			Model model) {
+		if(userCookie.equals("defaultCookieValue")) {
+			
+			
+			model.addAttribute("user", null);
+		} 
+		else {
+			User user = userService.getMyUserCookie(userCookie);
+			User user1 = userService.getUserByEmailAndProvider(user.getEmail(),user.getProvider());
+			user.setFullName(user.getFullName().replace('+', ' '));
+			model.addAttribute("user", user1);
+		}
+			
+		return "listcourse";
+	}
+	@RequestMapping("levelcourse")
+	public String Levelcourse(@CookieValue(value = "MY_USER", defaultValue = "defaultCookieValue") String userCookie,
+			Model model) {
+		if(userCookie.equals("defaultCookieValue")) {
+			
+			
+			model.addAttribute("user", null);
+		} 
+		else {
+			User user = userService.getMyUserCookie(userCookie);
+			User user1 = userService.getUserByEmailAndProvider(user.getEmail(),user.getProvider());
+			user.setFullName(user.getFullName().replace('+', ' '));
+			model.addAttribute("user", user1);
+		}
+			
+		return "levelcourse";
+	}
+	@RequestMapping("course_detail_view")
+	public String Course_detail_view(@CookieValue(value = "MY_USER", defaultValue = "defaultCookieValue") String userCookie,
+			Model model) {
+		if(userCookie.equals("defaultCookieValue")) {
+			
+			
+			model.addAttribute("user", null);
+		} 
+		else {
+			User user = userService.getMyUserCookie(userCookie);
+			User user1 = userService.getUserByEmailAndProvider(user.getEmail(),user.getProvider());
+			user.setFullName(user.getFullName().replace('+', ' '));
+			model.addAttribute("user", user1);
+		}
+			
+		return "course_detail_view";
+	}
 	@RequestMapping(value = "/res", method  = RequestMethod.POST)
 	public String ACC() {
 		return "home";
